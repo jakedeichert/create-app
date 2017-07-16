591735326a9c0419abb04ae2656333e451029425
Jake Deichert <git@jakedeichert.com>
2017-07-16 15:59:02 -0400

    Adjust cmd setup

--- cli/index.js
+++ cli/index.js
@@ -1,1 +1,26 @@
-require('yargs').commandDir('./cmds').help().version().argv;
+const yargs = require('yargs');
+
+yargs
+  .usage(
+    'create-app - a web app generator that abstracts away build configuration'
+  )
+  .commandDir('./cmds')
+  .option('v', {
+    alias: 'version',
+    global: false,
+  })
+  .option('h', {
+    alias: 'help',
+  })
+  .version()
+  .help('h').argv;
+
+// Must add this after otherwise catch all will prevent -v from working
+yargs.command({
+  command: '*',
+  handler: () => {
+    console.log('Missing command: run `create-app --help` for usage info');
+    // This doesn't work for some reason
+    // yargs.showHelp();
+  },
+}).argv;
