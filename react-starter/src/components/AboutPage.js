f6fc19aa9d0c73e3a672be88c966b716fe73c3e2
Jake Deichert <git@jakedeichert.com>
2018-03-17 19:00:28 -0400

    Update readme and links

--- react-starter/src/components/AboutPage.js
+++ react-starter/src/components/AboutPage.js
@@ -31,8 +31,8 @@ const AboutPage = () => (
     <Paragraph>
       Check it out here:
       <br />
-      <Link ext="https://github.com/jakedeichert/react-starter">
-        https://github.com/jakedeichert/react-starter
+      <Link ext="https://github.com/jakedeichert/create-app/tree/master/react-starter">
+        https://github.com/jakedeichert/create-app/tree/master/react-starter
       </Link>
     </Paragraph>
   </Wrapper>
