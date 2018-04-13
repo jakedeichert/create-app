84a443c14ab7b4b74572cccce2eec3bb73197d36
Jake Deichert <git@jakedeichert.com>
2018-04-12 23:33:04 -0400

    Fix babel preset setup for tests
    So lerna works

--- packages/create-app/lib/env-configs/react/jest.transform.js
+++ packages/create-app/lib/env-configs/react/jest.transform.js
@@ -3,15 +3,15 @@
 // from https://github.com/facebook/jest/issues/1468#issuecomment-276753756
 module.exports = require('babel-jest').createTransformer({
   presets: [
-    [
-      'env',
-      {
-        node: 'current',
-        // A little different than the normal babelrc (don't disable "modules")
-        // https://babeljs.io/docs/plugins/preset-env/#optionsmodules
-      },
-    ],
-    'react',
+    require('babel-preset-env').default({
+      node: 'current',
+      // A little different than the normal babelrc (don't disable "modules")
+      // https://babeljs.io/docs/plugins/preset-env/#optionsmodules
+    }),
+    require('babel-preset-react'),
   ],
-  plugins: ['transform-class-properties', 'transform-object-rest-spread'],
+  plugins: [
+    require('babel-plugin-transform-class-properties'),
+    require('babel-plugin-transform-object-rest-spread'),
+  ],
 });
