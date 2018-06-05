61ab8ee809ecaafe7b9f6275111b54bfe6d6ab41
Jake Deichert <git@jakedeichert.com>
2018-06-04 23:39:46 -0400

    Fix cleanup script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,5 +7,6 @@ echo "==========================================================="
 THIS_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 root_dir="$THIS_SCRIPTS_DIR/.."
 
-rm -rf */node_modules
-rm -rf */*/node_modules
+rm -rf $root_dir/node_modules
+rm -rf $root_dir/*/node_modules
+rm -rf $root_dir/*/*/node_modules
