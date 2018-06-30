e4384e38e01eb0e9300f02b51d5c7fba8adbf926
Jake Deichert <git@jakedeichert.com>
2018-06-30 11:13:39 -0400

    Adjust for eslint 5 upgrade

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -11,9 +11,7 @@ module.exports = {
 
   parserOptions: {
     sourceType: 'module',
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-    },
+    ecmaVersion: 2018,
   },
 
   extends: 'eslint:recommended',
