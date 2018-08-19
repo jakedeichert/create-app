bd88ca66192ca0a58183d9cff29fc64f01f8ef7f
Jake Deichert <git@jakedeichert.com>
2018-08-19 13:15:22 -0400

    Exit publish script if no NPM_CONFIG_OTP token

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,4 +8,10 @@ THIS_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$THIS_SCRIPTS_DIR/.."
 
 cd $ROOT_DIR
+
+if [[ "$NPM_CONFIG_OTP" == "" ]]; then
+    echo "ERROR: NPM_CONFIG_OTP should be set"
+    exit 1
+fi
+
 lerna publish
