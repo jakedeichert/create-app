94545948ee8dc872d496989f8dd11a6ae3b5cebd
Jake Deichert <git@jakedeichert.com>
2018-10-09 22:30:13 -0400

    Build a lib for node if an entrypoint exists

--- packages/create-app/lib/env-configs/webpack-common/entry.js
+++ packages/create-app/lib/env-configs/webpack-common/entry.js
@@ -1,7 +1,13 @@
 const path = require('path');
 
-module.exports = (config, workingDir, projectType) => {
+module.exports = (config, workingDir, projectType, isLibrary) => {
   const mainExt = projectType === 'typescript-react' ? 'tsx' : 'js';
+
+  if (isLibrary) {
+    config.entry = path.join(workingDir, `src/index.lib.${mainExt}`);
+    return;
+  }
+
   config.entry = {
     app: [path.join(workingDir, `src/index.${mainExt}`)],
   };
--- packages/create-app/lib/env-configs/webpack-common/output.js
+++ packages/create-app/lib/env-configs/webpack-common/output.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const { getBasePath } = require('../../utils/configHelpers');
 
-module.exports = (config, workingDir) => {
+module.exports = (config, workingDir, isLibrary) => {
   config.output = {
     path: path.join(workingDir, 'dist/'),
-    publicPath: getBasePath(),
-    filename: 'bundle/[name].[hash:8].bundle.js',
-    chunkFilename: 'bundle/[name].[chunkhash:8].bundle.js',
   };
+
+  if (isLibrary) {
+    config.output.libraryTarget = 'commonjs';
+    config.output.filename = 'lib.js';
+    return;
+  }
+
+  config.output.publicPath = getBasePath();
+  config.output.filename = 'bundle/[name].[hash:8].bundle.js';
+  config.output.chunkFilename = 'bundle/[name].[chunkhash:8].bundle.js';
 };
